e264b60947dd19afa4a69be04745fd816a6cc49b
Irina Kagan <irinagamb@gmail.com>
2019-05-12 19:09:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,4 +7,8 @@ class Teacher < User
                "class method class variable instance method instance varaible",
                "programming computers hacking learning terminal",
                "bash Ruby rvm update certs"]
-             end
+
+               def teach
+      KNOWLEDGE.sample
+    end
+  end
